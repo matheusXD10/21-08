3edda4b77d6f052c667950a1a98657a8351ccf73
matheusXD10 <limamatheushenrique95@gmail.com>
2024-08-22 13:44:17 -0300

    Create construtor_destruidor.php

---
+++ construtor_destruidor.php
@@ -0,0 +1,38 @@
+<?php
+
+//objeto novo
+class Pessoa {
+
+    //atributo nome para o objeto
+public $nome=null;
+
+//função construct para definir que o objeto foi iniciado
+function __construct($nome){
+    echo 'objeto iniciado';
+    $this->nome = $nome;
+
+}
+
+//função destruct para definir que o objeto doi destruído
+function __destruct(){
+    echo 'objeto removido'
+}
+
+//GET para colocar valor no $nome
+function __get($atributo) {
+    return $this->$atributo;
+}
+
+
+}
+
+//criamos a variável como uma nova pessoa, e setamos com o nome
+$pessoa = new Pessoa('Lucas'); 
+
+// concatenamos com o  __get "mágico"
+echo '<br> Nome:' . $pessoa->__get('nome') . '<br>'; 
+
+//foi removido o objeto, deixando a variavel pessoa indefinida
+unset($pessoa); 
+
+?>
